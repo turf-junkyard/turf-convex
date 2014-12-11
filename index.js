310b0796f432b947b466040853cf0efe6481320b
morganherlocker <morgan.herlocker@gmail.com>
2014-12-10 23:12:52 -0500

    add benching

--- index.js
+++ index.js
@@ -2,33 +2,33 @@
 
 module.exports = function(fc){
   var points = fc.features.map(function(point){
-    return point.geometry.coordinates
-  })
+    return point.geometry.coordinates;
+  });
 
   points.sort(function(a, b) {
-    return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
-  })
+    return a[0] == b[0] ? a[1] - b[1] : a[0] - b[0];
+  });
 
-  var lower = []
+  var lower = [];
   for (var i = 0; i < points.length; i++) {
     while (lower.length >= 2 && cross(lower[lower.length - 2], lower[lower.length - 1], points[i]) <= 0) {
-       lower.pop()
+       lower.pop();
     }
-    lower.push(points[i])
+    lower.push(points[i]);
   }
 
-  var upper = []
+  var upper = [];
   for (var i = points.length - 1; i >= 0; i--) {
     while (upper.length >= 2 && cross(upper[upper.length - 2], upper[upper.length - 1], points[i]) <= 0) {
-       upper.pop()
+       upper.pop();
     }
-    upper.push(points[i])
+    upper.push(points[i]);
   }
 
-  upper.pop()
-  lower.pop()
-  var coords = lower.concat(upper)
-  coords.push(coords[0])
+  upper.pop();
+  lower.pop();
+  var coords = lower.concat(upper);
+  coords.push(coords[0]);
   return {
     type:'Feature',
     properties: {},
@@ -38,9 +38,9 @@ module.exports = function(fc){
         coords
       ]
     } 
-  }
+  };
 }
 
 function cross(o, a, b) {
-   return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0])
+   return (a[0] - o[0]) * (b[1] - o[1]) - (a[1] - o[1]) * (b[0] - o[0]);
 }
